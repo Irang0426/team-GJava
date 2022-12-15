fd4e5ad9ef29bc268d5a8f28241a1ac6fcd4e212
massmess94 <zzanboy@gmail.com>
2022-12-15 19:36:01 +0900

    massmess first commit

---
+++ java-baseball/massmess/java-baseball/src/main/java/baseball/ComputerTest.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class ComputerTest {
+
+    public static void main(String[] args){
+
+        System.out.println(Computer.NumList());
+    }
+}
---
+++ java-baseball/massmess/java-baseball/src/main/java/baseball/UserTest.java
@@ -0,0 +1,8 @@
+package baseball;
+
+public class UserTest {
+
+    public static void main(String[] args) {
+        System.out.println(User.NumList());
+    }
+}
