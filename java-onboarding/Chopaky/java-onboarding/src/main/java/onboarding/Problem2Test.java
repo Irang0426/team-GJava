9b2b57c5607e891577d6cb5c8c432bc0a054448e
Chopaky <hommetuna@gmail.com>
2022-12-15 19:41:10 +0900

    Test

--- java-onboarding/Chopaky/java-onboarding/src/main/java/onboarding/Problem2Test.java
+++
@@ -1,8 +0,0 @@
-package onboarding;
-
-public class Problem2Test {
-
-    public static void main(String[] args) {
-        System.out.println( Problem2.solution("abccbba"));
-    }
-}
--- java-onboarding/Chopaky/java-onboarding/src/main/java/onboarding/Problem4Test.java
+++
@@ -1,9 +0,0 @@
-package onboarding;
-
-public class Problem4Test {
-    public static void main(String[] args) {
-
-        System.out.println(Problem4.solution("sdfjksd sdgfjskDFDFJ"));
-
-    }
-}
